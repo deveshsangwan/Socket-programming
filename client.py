9429de123b201b781653935ae49f06d64921bd69
deveshsangwan <dev.sangwan2001@gmail.com>
2020-04-26 23:48:22 +0530

    initial commit

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.226.1"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+send("Hello Muklord!")
+send(input())
+
+send(DISCONNECT_MESSAGE)
